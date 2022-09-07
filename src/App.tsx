bdb4e7452726f8c47af5f1b45c0bd896d8f95fb9
Matt Baltzer <baltzer.matt@gmail.com>
2022-09-07 16:36:37 -0700

    Created main page, added base search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ComponentProps } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Input } from "reactstrap";
+import logo from "./logo.svg";
+import Layout from "./layout";
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface GameInputProps {
+  name: string;
+  label: string;
+  placeholder: string;
+  errorMessage: string;
+  type: ComponentProps<typeof Input>["type"];
 }
 
-export default App;
+class GameInput extends React.Component<GameInputProps> {
+  render() {
+    return (
+      <Layout>
+        <div>
+          <header>
+            <h1>Game Deals Search</h1>
+          </header>
+        </div>
+        <div>
+          <Input
+            name={this.props.name}
+            label={this.props.label}
+            placeholder={this.props.placeholder}
+            type={this.props.type}
+          />
+        </div>
+      </Layout>
+    );
+  }
+}
+export default GameInput;
